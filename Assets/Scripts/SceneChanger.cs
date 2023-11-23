5725fb3f8baab131f48f651e11db5c8c9d27595f
Maria Nicole Bulalacao <B00157129@mytudublin.ie>
2023-11-23 14:27:44 +0000

    SCENECHANGERWORKS

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,9 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SceneChanger : MonoBehaviour
 {
-   
+    public void scene_changer(string Scene_WildCat)
+    {
+        SceneManager.LoadScene(Scene_WildCat);
+    }
 }
